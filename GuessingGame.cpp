b5d8092ff22f9c5bbe5fd3cbb4cc0b3af0189ba1
Mohammed Choudhury <mzchoudhury@outlook.com>
2021-07-21 14:30:00 +0100

    Guess counter and list of attempts functionality added

--- GuessingGame.cpp
+++ GuessingGame.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <windows.h>
 #include <typeinfo>
 #include <chrono>
@@ -8,6 +9,20 @@
 #include <ctime>
 
 using std::string;
+using std::vector;
+
+
+/**
+* Loops through array and prints out each element.
+* @param arr - Takes a vector of integers.
+*/
+void printArray(vector<int> arr)
+{
+	for (int i = 0; i < arr.size(); i++)
+	{
+		std::cout << arr[i] << "\t";
+	}
+}
 
 /**
 * Generates a random number and users have to guess the number.
@@ -15,16 +30,23 @@ using std::string;
 void playGame()
 {
 	int random = rand() % 251; //Generates a random number
+	vector<int> guesses;
+	int count = 0;
+
 	std::cout << "Guess a number: " << std::endl;
 
 	while (true)
 	{
 		int guess;
 		std::cin >> guess;
+		guesses.push_back(guess);
+		count++;
 
 		if (guess == random)
 		{
 			std::cout << "You win!" << std::endl;
+			std::cout << "It took you: " << count++ << " attempts!" << std::endl;
+			printArray(guesses);
 			Sleep(2000); //The program pauses for 2 seconds
 			//std::this_thread::sleep_for(std::chrono::seconds(2));
 			system("CLS"); //Clears the console window
